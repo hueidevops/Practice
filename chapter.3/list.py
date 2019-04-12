3411e4396cd9d440e7fedf01183dc2a565fab5a2
hueidevops <tn00787317@gmail.com>
2019-04-12 22:35:35 +0800

    done

---
+++ chapter.3/list.py
@@ -0,0 +1,14 @@
+# 串列 - 以 [ 來標示串列
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+
+# 索引足標是從 0開始
+print(bicycles[0])
+print(bicycles[0].title())
+
+# 存取串列的最後一個項目
+print(bicycles[-1])
+
+# 取用串列中的個別值
+message = "My first bicycle was a " + bicycles[0].title() + "."
+print(message)
