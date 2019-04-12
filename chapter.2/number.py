0a3288df634b3362f3c642d314c057ce9392f99e
hueidevops <tn00787317@gmail.com>
2019-04-12 14:53:51 +0800

    number done!

---
+++ chapter.2/number.py
@@ -0,0 +1,40 @@
+# 整數
+# # 加法
+num = 2 + 3
+print(num)
+
+# # 減法
+num = 3 - 2
+print(num)
+
+# # 乘法
+num = 2 * 3
+print(num)
+
+# # 除法
+num = 3 /2
+print(num)
+
+# # 次方
+num = 3 ** 2
+print(num)
+
+num2 = 3 ** 3
+print(num2)
+
+# ----------------------- #
+
+# 浮點數
+num = 0.2 + 0.1
+print(num)
+
+num = 2 * 0.1
+print(num)
+
+# ----------------------- #
+
+# 用str()避開型別錯誤
+
+age = 23
+message = "Happy " + str(age) + "rd Birthday!"
+print(message)
