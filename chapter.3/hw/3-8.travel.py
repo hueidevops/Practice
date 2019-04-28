a2eb4ed04635647514a970c85a10f9b9b8d0029c
Charles.Wang <tn00787317@gmail.com>
2019-04-28 14:38:33 +0800

    done

---
+++ chapter.3/hw/3-8.travel.py
@@ -0,0 +1,21 @@
+country = ['france', 'germany', 'japan', 'usa', 'egypt', 'turkey', 'india', 'cambodia']
+print(country)
+
+print(sorted(country))
+
+print(sorted(country, reverse=True))
+
+print(country)
+
+country.reverse()
+print(country)
+
+country.reverse()
+print(country)
+
+country.sort()
+print(country)
+
+country.sort(reverse=True)
+print(country)
+
