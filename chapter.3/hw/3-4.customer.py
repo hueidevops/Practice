8f00e739ff59ba5b38b47019534c0a715b5eea5b
Charles.Wang <tn00787317@gmail.com>
2019-05-01 17:33:52 +0800

    新增迴圈方式

--- chapter.3/hw/3-4.customer.py
+++ chapter.3/hw/3-4.customer.py
@@ -1,7 +1,10 @@
-customer = ['charles', 'valerie', 'jason', 'ben', 'jasper', 'andy']
+customers = ['charles', 'valerie', 'jason', 'ben', 'jasper', 'andy']
 
-i = 0
-while i < 6:
+# i = 0
+# while i < 6:
     # u -> 輸出中文
-    print(u"Hi " + customer[i].title() + ", 要不要一起來吃個飯呢？")
-    i += 1
+    # print(u"Hi " + customers[i].title() + ", 要不要一起來吃個飯呢？")
+    # i += 1
+
+for customer in customers:
+    print(u"Hi " + customer.title() + ", 要不要來一起來個飯？")
