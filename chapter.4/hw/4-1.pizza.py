eeda378bf99c121554e3e8fc2bffcd0fe9eb11ac
Charles.Wang <tn00787317@gmail.com>
2019-05-01 17:42:14 +0800

    修改內容

--- chapter.4/hw/4-1.pizza.py
+++ chapter.4/hw/4-1.pizza.py
@@ -1,5 +1,8 @@
 pizza = ['海鮮', '夏威夷', '地中海漁夫', '燻雞']
 
 for favorite_pizza in pizza:
+    print(favorite_pizza)
+
+for favorite_pizza in pizza:
     print(u"我最愛的Pizza口味是：" + favorite_pizza)
 print(u"\n我真的最愛Pizza")
