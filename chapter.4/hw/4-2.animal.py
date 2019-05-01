f88b9826dc8591f903aa9b61ddce8e21613e80a9
Charles.Wang <tn00787317@gmail.com>
2019-05-01 17:44:15 +0800

    done

---
+++ chapter.4/hw/4-2.animal.py
@@ -0,0 +1,9 @@
+animals = ['dog', 'cat', 'bird', 'fish', 'mouse']
+
+for animal in animals:
+    print(animal)
+
+for animal in animals:
+    print('A ' + animal + ' would make a great pet.')
+
+print('\nAny of these animals would make a great pet.')
